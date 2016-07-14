262e1456b2748d964a29f0da52ac5d80ff7953d1
Michael Delgado <mdelgado@rhg.com>
2016-07-13 19:44:08 -0700

    fix encoding issues for python-27

--- metacsv/io/to_csv.py
+++ metacsv/io/to_csv.py
@@ -1,7 +1,7 @@
 
 from collections import OrderedDict
 from .yaml_tools import ordered_dump
-from .._compat import string_types, has_iterkeys, iterkeys
+from .._compat import string_types, has_iterkeys, iterkeys, text_type, text_to_native
 
 
 def _header_to_file_object(fp, attrs=None, coords=None, variables=None):
@@ -18,13 +18,14 @@ def _header_to_file_object(fp, attrs=None, coords=None, variables=None):
         attr_dict.update({'variables': variables._data})
 
     if len(attr_dict) > 0:
-        fp.write('---\n')
-        fp.write(ordered_dump(
-            attr_dict, default_flow_style=False, allow_unicode=True))
-        fp.write('...\n')
+        fp.write(text_to_native(('---\n'), 'utf-8'))
+        fp.write(text_to_native(ordered_dump(
+            attr_dict, default_flow_style=False, allow_unicode=True), 'utf-8'))
+        fp.write(text_to_native(('...\n'), 'utf-8'))
 
 def _container_to_csv_object(container, fp, *args, **kwargs):
-    container.pandas_parent.to_csv(container, fp, *args, **kwargs)
+    encoding = kwargs.pop('encoding', 'utf-8')
+    container.pandas_parent.to_csv(container, fp, *args, encoding=encoding, **kwargs)
 
 def metacsv_to_csv(container, fp, header_file=None, *args, **kwargs):
     separate_header = False
@@ -36,7 +37,7 @@ def metacsv_to_csv(container, fp, header_file=None, *args, **kwargs):
         metacsv_to_header(header_file, attrs=container.attrs, coords=container.coords, variables=container.variables)
 
     if isinstance(fp, string_types):
-        with open(fp, 'w+') as fp2:
+        with open(text_type(fp), 'w+') as fp2:
             if not separate_header:
                 _header_to_file_object(fp2, attrs=container.attrs, coords=container.coords, variables=container.variables)
             _container_to_csv_object(container, fp2, *args, **kwargs)
@@ -47,7 +48,7 @@ def metacsv_to_csv(container, fp, header_file=None, *args, **kwargs):
 
 def metacsv_to_header(fp, attrs=None, coords=None, variables=None):
     if isinstance(fp, string_types):
-        with open(fp, 'w+') as fp2:
+        with open(text_type(fp), 'w+') as fp2:
             _header_to_file_object(fp2, attrs=attrs, coords=coords, variables=variables)
     else:
         _header_to_file_object(fp, attrs=attrs, coords=coords, variables=variables)
