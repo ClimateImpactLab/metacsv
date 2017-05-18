60dd5e926b68709dd8ef8cf1aeb1fd2a329a65ed
Michael Delgado <mdelgado@rhg.com>
2017-05-17 21:06:08 -0700

    drop python=3.4 support (xarray>=0.9.5 no longer supports)

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -77,8 +75,8 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     test_suite='metacsv.testsuite',
 )
